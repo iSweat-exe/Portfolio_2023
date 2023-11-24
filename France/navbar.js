a890a817d66c7583d60c37e115c8b26872f65d18
isweat-exe <platiniumsamu@gmail.com>
2023-11-24 23:22:27 +0100

    Home page CSS

--- France/navbar.js
+++ France/navbar.js
@@ -1,13 +1,17 @@
-// navnar.js
-
 const navMobile = document.querySelector(".nav-mobile");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const closeMenu = document.querySelector(".close-menu");
+const html = document.documentElement;
+const body = document.body;
 
 console.log("Chargé !");
 
 function toggleMenu() {
     navMobile.classList.toggle("active");
+
+    // Ajouter ou supprimer la classe 'no-scroll' sur html et body
+    html.classList.toggle("no-scroll");
+    body.classList.toggle("no-scroll");
 }
 
 hamburgerMenu.addEventListener("click", () => {
